3120d1c8a62a7acc0131756d273751d4a6efae86
Nick Sutterer <apotonick@gmail.com>
2017-07-29 14:33:41 +0200

    cleanup helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,6 @@
 ENV['RACK_ENV'] = 'test'
 require "minitest/autorun"
+require 'rack/test'
 
 require_relative "../application"
 
-require 'rack/test'
-Dir['./test/support/*.rb'].each { |file| require file }
-
-# require "match_json"
