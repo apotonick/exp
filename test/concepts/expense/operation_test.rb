e0143c033da7472d3b1a96444a236c6d4be75af6
Nick Sutterer <apotonick@gmail.com>
2017-08-17 21:09:32 +0200

    move code to trb-test.

--- test/concepts/expense/operation_test.rb
+++ test/concepts/expense/operation_test.rb
@@ -1,27 +1,5 @@
 require "test_helper"
 
-
-module Trailblazer::Test::Operation
-  module Assertions
-    def assert_passes(operation_class, params, expected_attributes, &block)
-      default_params = params_valid
-      default_attributes = attributes_valid # FIXME.
-
-      result = operation_class.( default_params.merge(params) )
-
-      assert_result_passes(result, expected_attributes, &block)
-    end
-
-    def assert_result_passes(result, expected_attributes={}, &block) # TODO: test expected_attributes default param and explicit!
-      assert_equal true, result.success?
-
-      return yield result if block_given?  # DISCUSS: result or model?
-
-      assert_exposes( result["model"], expected_attributes )
-    end
-  end
-end
-
 class ExpenseOperationTest < Minitest::Spec
   let(:params_valid) do
     { source: "Biosk", description: "Beer", unit_price: "1.2", currency: "EUR",
